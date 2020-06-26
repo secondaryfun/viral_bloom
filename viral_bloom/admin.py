04266a810ff80a126ad616be49ec861fe308c9fc
secondaryfun <c.birkenhagen@gmail.com>
2020-06-26 14:15:31 -0400

    changed model name

--- viral_bloom/admin.py
+++ viral_bloom/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import State
+from .models import CovidDataByDate
 
 # Register your models here.
-admin.site.register(State)
+admin.site.register(CovidDataByDate)
