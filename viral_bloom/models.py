3aceaeca183812d5044d9ad337ccad1f5659e85b
secondaryfun <c.birkenhagen@gmail.com>
2020-06-26 14:39:01 -0400

    adding seed loop to models

--- viral_bloom/models.py
+++ viral_bloom/models.py
@@ -12,7 +12,9 @@ class CovidDataByDate(models.Model):
     def __str__(self):
         return self.state + " - " + self.date
 
-# with open('./daily.csv', newline='') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     for row in reader:
-#         CovidDataByDate(date=row['date'], state=row['state'], positive=row['positive'], death=row['death'], recovered=row['recovered']).save()
+
+with open('./daily.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        CovidDataByDate(date=row['date'], state=row['state'], positive=row['positive'],
+                        death=row['death'], recovered=row['recovered']).save()
